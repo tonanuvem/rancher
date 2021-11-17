9067c885d0ce9ab2768e64ac4869c98dcaa70e41
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-11-17 13:53:51 -0300

    Update install_rancher_server.sh

--- install_rancher_server.sh
+++ install_rancher_server.sh
@@ -4,6 +4,7 @@ echo "Instalando o Rancher Server"
 
 sudo docker run --name rancherserver --privileged -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
 
+echo ""
 echo "Aguardando a configuração do Rancher."
 
 while [ "$(docker logs rancherserver 2>&1 | grep "Bootstrap Password:" | wc -l)" != "1" ]; do
@@ -11,13 +12,14 @@ while [ "$(docker logs rancherserver 2>&1 | grep "Bootstrap Password:" | wc -l)"
   sleep 1
 done
 
+echo ""
+echo ""
 echo "Senha de Bootstrap:"
 echo ""
 
 docker logs rancherserver 2>&1 | grep "Bootstrap Password:"
 
-
 echo "URL de acesso:"
 echo ""
-
 echo https://fiap.$(curl -s checkip.amazonaws.com).nip.io
+echo ""
