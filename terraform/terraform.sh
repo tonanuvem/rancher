7d4eb4b869c229f8326de3899f4bad4a5ad7dc27
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-04-23 15:26:30 -0300

    Create terraform.sh

---
+++ terraform/terraform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Atualizar versao do Terraform: 
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tTerraform:\n\n"
+curl -s "https://releases.hashicorp.com/terraform/1.8.0/terraform_1.8.0_linux_amd64.zip" -o "terraform_linux_amd64.zip"
+unzip terraform_linux_amd64.zip
+sudo mv terraform /usr/bin/
+
+# Verificando as versões instaladas e atualizar permissão docker:
+cd ~
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tVerificando as instações:\n\n"
+
+printf "\n\tTERRAFORM:\n"
+terraform --version
