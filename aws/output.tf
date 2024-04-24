7bd8fe4ec1cd1dc4445a350cbc14fa6084cf7b67
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-04-24 10:23:28 -0300

    Update output.tf

--- aws/output.tf
+++ aws/output.tf
@@ -2,10 +2,22 @@ output "rancher_server_url" {
   value = module.rancher_common.rancher_url
 }
 
-output "rancher_node_ip" {
-  value = aws_instance.rancher_server.public_ip
+output "usuario_login {
+  value = "admin"
 }
 
-output "workload_node_ip" {
-  value = aws_instance.quickstart_node.public_ip
+output "senha_login" {
+  value = "fiapadmin123"
+}
+
+output "fiap_master_ip" {
+  value = aws_instance.fiap_master.public_ip
+}
+
+output "fiap_worker1_ip" {
+  value = aws_instance.fiap_worker1.public_ip
+}
+
+output "fiap_worker2_ip" {
+  value = aws_instance.fiap_worker2.public_ip
 }
