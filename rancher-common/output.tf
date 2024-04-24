e0674b3ec728696126ec0b6bffcc23e0f43356c7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-04-24 17:23:27 -0300

    Update output.tf

--- rancher-common/output.tf
+++ rancher-common/output.tf
@@ -9,7 +9,7 @@ output "custom_cluster_command" {
   description = "Docker command used to add a node to the quickstart cluster"
 }
 
-output "custom_cluster_windows_command" {
-  value       = rancher2_cluster_v2.quickstart_workload.cluster_registration_token.0.insecure_windows_node_command
-  description = "Docker command used to add a windows node to the quickstart cluster"
-}
+#output "custom_cluster_windows_command" {
+#  value       = rancher2_cluster_v2.quickstart_workload.cluster_registration_token.0.insecure_windows_node_command
+#  description = "Docker command used to add a windows node to the quickstart cluster"
+#}
