b76d6e018c6a48efb9f100f4240bd6a0e670b976
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-10-02 18:16:49 -0300

    Update destruir.sh

--- aws/destruir.sh
+++ aws/destruir.sh
@@ -1,13 +1,13 @@
-RANCHER_INSTANCE_ID=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --output text --filters 'Name=tag:Name,Values=*rancher-server')
-echo "RANCHER_INSTANCE_ID = $RANCHER_INSTANCE_ID. Finalizando o Rancher server..."
-echo ""
-aws ec2 terminate-instances --instance-ids $RANCHER_INSTANCE_ID
-echo ""
+#RANCHER_INSTANCE_ID=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --output text --filters 'Name=tag:Name,Values=*rancher-server')
+#echo "RANCHER_INSTANCE_ID = $RANCHER_INSTANCE_ID. Finalizando o Rancher server..."
+#echo ""
+#aws ec2 terminate-instances --instance-ids $RANCHER_INSTANCE_ID
+#echo ""
 
 RANCHER_VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --output text --filters 'Name=tag:Name,Values=*rancher-vpc')
 echo "RANCHER_INSTANCE_ID = $RANCHER_VPC_ID. Finalizando o Rancher server..."
 echo ""
-aws ec2 delete-vpc --vpc-id $RANCHER_VPC_ID
+delete-vpc.sh --region us-east-1 --vpc-id $RANCHER_VPC_ID
 echo ""
 
 echo "Finalizando o restante do projeto"
