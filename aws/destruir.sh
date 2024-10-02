1940ea983549ec09a225a258ad5a92f9190eeb05
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-10-02 18:17:29 -0300

    Update destruir.sh

--- aws/destruir.sh
+++ aws/destruir.sh
@@ -7,7 +7,7 @@
 RANCHER_VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --output text --filters 'Name=tag:Name,Values=*rancher-vpc')
 echo "RANCHER_INSTANCE_ID = $RANCHER_VPC_ID. Finalizando o Rancher server..."
 echo ""
-delete-vpc.sh --region us-east-1 --vpc-id $RANCHER_VPC_ID
+delete_vpc.sh --region us-east-1 --vpc-id $RANCHER_VPC_ID
 echo ""
 
 echo "Finalizando o restante do projeto"
