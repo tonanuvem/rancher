85d530cdd81431134eeba89bd6f06df898e81a13
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-10-02 18:11:43 -0300

    Update destruir.sh

--- aws/destruir.sh
+++ aws/destruir.sh
@@ -3,6 +3,13 @@ echo "RANCHER_INSTANCE_ID = $RANCHER_INSTANCE_ID. Finalizando o Rancher server..
 echo ""
 aws ec2 terminate-instances --instance-ids $RANCHER_INSTANCE_ID
 echo ""
+
+RANCHER_VPC_ID=$(aws ec2 describe-vpcs --query "Vpcs[].VpcId" --output text --filters 'Name=tag:Name,Values=*rancher-vpc')
+echo "RANCHER_INSTANCE_ID = $RANCHER_VPC_ID. Finalizando o Rancher server..."
+echo ""
+aws ec2 delete-vpc --vpc-id $RANCHER_VPC_ID
+echo ""
+
 echo "Finalizando o restante do projeto"
 echo ""
 terraform init && terraform destroy -auto-approve
