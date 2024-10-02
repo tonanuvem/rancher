6e93f7100be71717010dbfab8b09b77bf98678f4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-10-02 17:53:42 -0300

    Update destruir.sh

--- aws/destruir.sh
+++ aws/destruir.sh
@@ -1,1 +1,8 @@
+RANCHER_INSTANCE_ID=$(aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" --output text --filters 'Name=tag:Name,Values=*rancher-server')
+echo "RANCHER_INSTANCE_ID = $RANCHER_INSTANCE_ID. Finalizando o Rancher server..."
+echo ""
+aws ec2 terminate-instances --instance-ids $RANCHER_INSTANCE_ID
+echo ""
+echo "Finalizando o restante do projeto"
+echo ""
 terraform init && terraform destroy -auto-approve
